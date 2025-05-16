7fb7e18d4e70025f2caf68bad3bde98fb0a77722
Doru Arfire <doru@nonlinearsoft.com>
2025-05-16 22:26:21 +0200

    - Removed tmux wrapper.

--- scripts/start_tmux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tmux new-session -d -s "layosh" -n "main" "./layosh server -debug -session 1 $@" &&
-tmux split-window -h -t "layosh:main" './layosh client -debug --mode shell -session 1' &&
-tmux split-window -v -t "layosh:main" './layosh client -debug --mode llm -session 1'
-
-tmux attach -t "layosh:main"
